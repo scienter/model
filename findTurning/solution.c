c01a17ad8d3c99211aaaf2879f8cd41a85607305
Jo <scienter@daum.net>
2015-09-17 00:41:17 +0900

    find

--- findTurning/solution.c
+++ findTurning/solution.c
@@ -14,27 +14,25 @@ void main(int argc, char *argv[])
   
   if(argc<5)
   {
-    printf("solution [final] [saveStep] [energy0] [dt] [density]\n");
+    printf("solution [final] [x0] [energy0] [dt] [density]\n");
     exit(0);
   }
   final=atoi(argv[1]);
-  saveStep=atoi(argv[2]);
+  x0=atof(argv[2]);
   energy0=atof(argv[3]);
   dt=atof(argv[4]);
   density=atof(argv[5]);
 
-
-  x0=50e-9;
   m=9.11e-31;
   c=3e8;
   e=1.602e-19;
   
 //  p0=sqrt(energy0*2.0*m*e);
-  gamma=energy0/0.511e6+1.0;
-  p0=sqrt(gamma*gamma-1.0)*m*c;
+  energy0=energy0*e;
+  p0=sqrt(energy0*energy0/c/c+2.0*energy0*m);
 
   out=fopen("Ex","w");
-  for(x=x0; x<=2e-5; x+=1e-7)
+  for(x=0; x<=100*50e-9; x+=10e-9)
   {
     field=Efield(x,density);
     fprintf(out,"%g %g\n",x,field);
@@ -42,15 +40,16 @@ void main(int argc, char *argv[])
   fclose(out);
 
 
-  x=xold=x0;
+  x=x0;
+  p=p0;
   for(i=0; i<=final; i++)
   {
     t=dt*i;
-    p=momentum(0.5*(x+xold),t+0.5*dt,p0,density);
+    field=e*Efield(x,density);
+    p+=field*dt;
     gamma=sqrt(1.0+p*p/m/m/c/c);
-    xold=x;
-    x=xold+p/m/gamma*dt;
-    printf("%g %g %g\n",x,p,gamma);
+    x+=p/gamma/m*dt;
+    printf("%g %g %g\n",x,p/m/c,gamma);
   }
 }
 
@@ -69,12 +68,16 @@ float momentum(float x,float t,float p0,float n)
 
 float Efield(float x,float n)
 {
-  float e,eps0,A,result;
+  float e,eps0,A,result,incline;
 
   e=1.601e-19;
   eps0=8.85e-12;
   A=50e-9*50e-9*3.14;
-  result=0.5/3.14/eps0*e*n*A/x;
+  incline=-0.5/3.14/eps0*e*n*A/50e-9;
+  if(x>=50e-9)
+    result=-0.5/3.14/eps0*e*n*A/x;
+  else if(x>=0.0 && x<50e-9)
+    result=incline*x;
 
   return result;
 }  
