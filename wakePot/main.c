8b484590023713fc42d113412eeda1ba969d6b5d
scienter <scienter@daum.net>
2015-09-24 18:42:27 +0900

    wake

--- wakePot/main.c
+++ wakePot/main.c
@@ -6,18 +6,20 @@
 #include "constants.h"
 
 float laser(float x,float sigma,float a0);
+float updatePx(float k0,float alpha,float r0,float x0,float x,float t);
 
 int main(int argc, char *argv[])
 {
    float a0,sigma,density,rangeX,wp,kp,minX,dx,x,a,pot,E,px;
+   float r0,n_n,k0,alpha,gamma,t,dt,f,x0,Enano; 
    float *phi;
-   int Nx,i;
+   int Nx,i,maxStep;
    char name[100];
    FILE *out;
 
-   if(argc<5)
+   if(argc<10)
    {
-     printf("show a0 sigma density rangeX Nx\n");
+     printf("show a0 sigma density rangeX Nx finalTime r0 n_n k0 nanoX0\n");
      exit(0);
    }
    
@@ -26,16 +28,26 @@ int main(int argc, char *argv[])
    density=atof(argv[3]);
    rangeX=atof(argv[4]);
    Nx=atoi(argv[5]);
+   maxStep=atoi(argv[6]);
+   r0=atof(argv[7]);
+   n_n=atof(argv[8]);
+   k0=atof(argv[9]);
+   x0=atof(argv[10]);
 
    wp=sqrt(density*eCharge*eCharge/eMass/eps0);
    kp=wp/velocityC;
    sigma=sigma*kp;
    minX=-rangeX*kp;
+   r0=r0*kp;
+   alpha=0.5/pi*n_n/density*pi*r0*r0;
 
    dx=rangeX*kp/((float)Nx);
    phi=(float *)malloc((Nx+1)*sizeof(float));
+   dt=r0*0.1;
    for(i=0; i<=Nx; i++)
+   {
      phi[i]=0.0;
+   }
 
    //calculate potential
    for(i=Nx-2; i>=0; i--)
@@ -45,6 +57,21 @@ int main(int argc, char *argv[])
      phi[i]=0.5*dx*dx*((1.0+a*a)/(1.0+phi[i+1])/(1.0+phi[i+1])-1.0)-phi[i+2]+2.0*phi[i+1];
    }
 
+   //calculate Px
+   sprintf(name,"px%g",x0);
+   out=fopen(name,"w");
+   px=x=0.0;
+   for(i=0; i<=maxStep; i++)
+   { 
+     gamma=sqrt(1.0+px);
+     x+=px/gamma*dt;
+     t=i*dt+0.5*dt;
+     f=updatePx(k0,alpha,r0,x0,x,t);
+     px+=f*dt;
+     fprintf(out,"%g %g\n",x,px);
+   }
+   fclose(out);
+
    //save laser  
    out=fopen("result","w"); 
    for(i=0; i<Nx; i++)
@@ -53,6 +80,9 @@ int main(int argc, char *argv[])
      a=laser(x,sigma,a0);
      pot=phi[i];
      E=-(phi[i+1]-phi[i])/dx;
+     Enano=-alpha/(r0-(x-x0));
+     if(x>=x0-r0)  Enano=0.0;
+     E=E+Enano;
      px=0.5*((1.0+a*a)/(1.0+pot)-(1.0+pot));
      fprintf(out,"%g %g %g %g %g\n",x,a,E,-pot,px);
    }
@@ -70,4 +100,12 @@ float laser(float x,float sigma,float a0)
    x0=4.0*sigma;
    result=a0*exp(-(x+x0)*(x+x0)/sigma/sigma);
    return result;
+}
+
+float updatePx(float k0,float alpha,float r0,float x0,float x,float t)
+{
+  float result;
+
+  result=-0.5*k0*(x+x0-t)+alpha/(r0-x);
+  return result;
 }
